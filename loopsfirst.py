dc32235c4264ccf3612350ce1528748d9febfb5a
Eryck Ferreira <132472671+eryckferreira@users.noreply.github.com>
2025-01-22 19:27:45 -0300

    Add files via upload

---
+++ loopsfirst.py
@@ -0,0 +1,9 @@
+# Loop em números
+for number in range(1, 20):
+    print(number)
+
+# Loop em letras
+palavra = 'fantastica'
+
+for letter in 'google':
+    print(f'{letter} esta ferramenta é {palavra}')
---
+++ main.py
@@ -0,0 +1,6 @@
+valor = int(input("Insira um valor: "))
+
+if valor >= 20 and valor < 40:
+    print("Produto foi aceito")
+else:
+    print("Produto não aceito")
