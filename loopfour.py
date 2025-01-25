3a8c46d9a839a57d2e929107a5e787c910bf8e4e
Eryck Ferreira <132472671+eryckferreira@users.noreply.github.com>
2025-01-24 21:51:20 -0300

    Add files via upload

---
+++ loopfour.py
@@ -0,0 +1,11 @@
+valor1 = int(input('insira valor 1: '))
+valor2 = int(input('insira valor 2: '))
+
+resultado = valor1 + valor2
+
+print(f'o resultado dos dois valores é {resultado}')
+
+if valor1 >= 20 and valor2 >= 10:
+    print('Valor 1 acima!')
+else:
+    print('Valor abaixo!')
